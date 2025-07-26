ec0fc944cc73e9575d4b235ab7950356ba88981b
Jaspet Home <jaspethome@192.168.1.36>
2025-07-26 18:34:39 +0300

    Supabase URL düzeltildi

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://<https://dzidefhirlxofvmnsxyi.supabase.co>.supabase.co'
-const supabaseAnonKey = '<eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6aWRlZmhpcmx4b2Z2bW5zeHlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1MzgzMTYsImV4cCI6MjA2OTExNDMxNn0.ExHePdB21T7uCug9teLpoR1y66k11W21Pf4MblYr2jI>'
+const supabaseUrl = 'https://dzidefhirlxofvmnsxyi.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBoYWJhc2Uta2V5IiwiaWF0IjoxNjkyODg2MDI3LCJleHAiOjE5MDU0NDIwMjcsInN1YiI6ImY2NjY1ZGNkLTkzN2ItNDM3ZC1iNWE3LWQ0MmJmYTAxNjUwZSJ9.79k0DhJyaIzChZbhIhxR5nQG93aS4_Rf6iOR9M0bWSo'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
